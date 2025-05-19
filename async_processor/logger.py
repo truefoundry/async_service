f2c7654837f8fc594b3cb2e817ab7d77a571ee33
Akash Gupta <akash@truefoundry.com>
2025-05-19 18:07:38 +0530

    Add logging for kafka and gauge for fetch time (#111)
    * add debug log for kafka
    
    * Add guage for input fetch time
    
    * Fix logging setup
    
    ---------
    
    Co-authored-by: Chirag Jain <jain.chirag925@gmail.com>

--- async_processor/logger.py
+++ async_processor/logger.py
@@ -1,15 +1,23 @@
 import logging
 import os
+import sys
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 log_level = logging.getLevelName(LOG_LEVEL.upper())
 
-logger = logging.getLogger("async_processor")
-logger.setLevel(log_level)
-handler = logging.StreamHandler()
-handler.setLevel(log_level)
-formatter = logging.Formatter(
-    "%(asctime)s:%(name)s:%(levelname)s:%(process)d:%(module)s:%(funcName)s:%(lineno)d - %(message)s"
-)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+def _setup_logging(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(log_level)
+    formatter = logging.Formatter(
+        "%(asctime)s:%(name)s:%(levelname)s:%(process)d:%(module)s:%(funcName)s:%(lineno)d - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+logger = _setup_logging("async_processor")
+_setup_logging("kafka")
